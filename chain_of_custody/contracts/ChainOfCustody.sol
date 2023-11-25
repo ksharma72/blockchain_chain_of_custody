c0e9af58dce1c85b7feff9b2f1c4fe48d80573b5
unknown <angirekulasaketh@gmail.com>
2023-11-25 08:44:46 -0700

    implemented bchoc add functionality

--- chain_of_custody/contracts/ChainOfCustody.sol
+++ chain_of_custody/contracts/ChainOfCustody.sol
@@ -8,8 +8,8 @@ contract ChainOfCustody {
         uint128 caseId;
         uint32 evidenceItemId;
         bytes12 state;
-        bytes20 handlerName;
-        bytes20 organizationName;
+        string handlerName; // Changed from bytes20 to string
+        string organizationName; // Changed from bytes20 to string
         uint32 dataLength;
         bytes data;
     }
@@ -17,17 +17,19 @@ contract ChainOfCustody {
     Block[] public blockchain;
     mapping(uint32 => bool) public evidenceExists;
 
-    // Event declaration
-    event EvidenceItemAdded(uint128 caseId, uint32 evidenceItemId, uint64 timestamp, bytes12 state);
+    // Updated event to use strings
+    event EvidenceItemAdded(uint128 caseId, uint32 evidenceItemId, uint64 timestamp, bytes12 state, string handlerName, string organizationName);
 
-    function addEvidenceItems(uint128 _caseId, uint32[] memory _itemIds) public {
+    // Updated function to accept string parameters
+    function addEvidenceItems(uint128 _caseId, uint32[] memory _itemIds, string memory _handlerName, string memory _organizationName) public {
         for (uint i = 0; i < _itemIds.length; i++) {
             require(!evidenceExists[_itemIds[i]], "Evidence item ID must be unique");
-            addBlock(_caseId, _itemIds[i], "CHECKEDIN", "", "", "");
+            addBlock(_caseId, _itemIds[i], "CHECKEDIN", _handlerName, _organizationName, "");
         }
     }
 
-    function addBlock(uint128 _caseId, uint32 _evidenceItemId, bytes12 _state, bytes20 _handlerName, bytes20 _organizationName, bytes memory _data) private {
+    // Updated function to accept string parameters
+    function addBlock(uint128 _caseId, uint32 _evidenceItemId, bytes12 _state, string memory _handlerName, string memory _organizationName, bytes memory _data) private {
         bytes32 previousHash = blockchain.length > 0 ? getLatestBlockHash() : bytes32(0);
         uint64 timestamp = uint64(block.timestamp);
         uint32 dataLength = uint32(_data.length);
@@ -47,8 +49,7 @@ contract ChainOfCustody {
         blockchain.push(newBlock);
         evidenceExists[_evidenceItemId] = true;
 
-        // Emitting the event
-        emit EvidenceItemAdded(_caseId, _evidenceItemId, timestamp, _state);
+        emit EvidenceItemAdded(_caseId, _evidenceItemId, timestamp, _state, _handlerName, _organizationName);
     }
 
     function getLatestBlockHash() private view returns (bytes32) {
--- chain_of_custody/scripts/addEvidenceItem.js
+++ chain_of_custody/scripts/addEvidenceItem.js
@@ -1,43 +1,53 @@
 const Web3 = require('web3');
 const contractABI = require('../build/contracts/ChainOfCustody.json');
-const contractConfig = require('../build/contracts/ChainOfCustody.json');
-const contractAddress = contractConfig.contractAddress;
+
+// Assuming you are using Ganache, which typically uses network ID '5777'
+const networkId = '5777';
+const contractData = contractABI.networks[networkId];
+
+if (!contractData || !contractData.address) {
+    console.error("Contract not deployed on the current network (network ID: " + networkId + ")");
+    process.exit(1);
+}
+
+const contractAddress = contractData.address;
 
 // Create a web3 instance with the URL of your Ethereum node
-const web3 = new Web3('http://localhost:7545');
-
+const web3 = new Web3('http://127.0.0.1:7545');
 const contract = new web3.eth.Contract(contractABI.abi, contractAddress);
 
-async function addEvidenceItems(caseId, itemIds) {
+async function addEvidenceItems(caseId, itemIds, handler, organization) {
     const accounts = await web3.eth.getAccounts();
     const account = accounts[0]; // Using the first account for transactions
 
     try {
-        let result = await contract.methods.addEvidenceItems(caseId, itemIds).send({ from: account });
+        let result = await contract.methods.addEvidenceItems(caseId, itemIds, handler, organization).send({
+            from: account,
+            gas: 5000000 // Setting a higher gas limit
+        });
+
         let receipt = await web3.eth.getTransactionReceipt(result.transactionHash);
 
         if (receipt && receipt.logs) {
             receipt.logs.forEach((log) => {
-                // Adjusted to handle indexed parameters correctly
-                let event = web3.eth.abi.decodeLog([
-                    { type: 'uint64', name: 'timestamp' },
-                    { type: 'bytes12', name: 'state' }
-                ], log.data, log.topics.slice(1));  // Using slice(1) to skip the event signature hash
-
-                // Extracting indexed parameters directly from log topics
-                const caseId = web3.utils.toBN(log.topics[1]).toString();
-                const evidenceItemId = web3.utils.toBN(log.topics[2]).toString();
-
+                // Decoding log data... (update this part if necessary)
                 console.log("Evidence Item Added:");
                 console.log("Case ID:", caseId);
-                console.log("Item ID:", evidenceItemId);
-                console.log("Timestamp:", new Date(event.timestamp * 1000).toISOString());
-                console.log("State:", web3.utils.hexToAscii(event.state));
+                console.log("Item ID:", itemIds.join(", "));
+                console.log("Handler:", handler);
+                console.log("Organization:", organization);
+                // Additional log information...
             });
         }
     } catch (error) {
-        console.error('Error adding evidence items:', error);
+        console.error('Error adding evidence items:', error.message);
     }
 }
 
-addEvidenceItems(caseId, itemIds);
+// Accepting command line arguments
+const caseId = process.argv[2];
+const itemIds = process.argv[3].split(','); // Splitting item IDs into an array
+const handler = process.argv[4]; // Handler
+const organization = process.argv[5]; // Organization
+
+addEvidenceItems(caseId, itemIds, handler, organization).then(() => process.exit(0));
