11ad933a7c039526312bcd207a599b0cab445e6d
Kanishk Sharma <ksharm72@asu.edu>
2023-11-30 16:14:31 -0700

    fixes

--- chain_of_custody/scripts/init.js
+++ chain_of_custody/scripts/init.js
@@ -9,7 +9,6 @@ const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
 
 ChainOfCustody.setProvider(web3.currentProvider);
 
-// Function to get the contract address
 function getContractAddress() {
   const networkId = "5777";
   const contractData = ChainOfCustodyArtifact.networks[networkId];
@@ -22,7 +21,6 @@ function getContractAddress() {
   return contractData.address;
 }
 
-// Function to check if a port is open
 async function isPortOpen(port) {
   return new Promise((resolve) => {
     const net = require("net");
@@ -42,9 +40,7 @@ async function isPortOpen(port) {
 async function runTruffleMigrate() {
   return new Promise((resolve, reject) => {
     console.log("Running Truffle migrate...");
-    const truffleMigrate = spawn("truffle", ["migrate", "--reset"], {
-      shell: true,
-    });
+    const truffleMigrate = spawn("truffle", ["migrate", "--reset"], { shell: true });
 
     truffleMigrate.on("exit", (code) => {
       if (code === 0) {
@@ -64,37 +60,26 @@ async function checkForBlocks() {
 
     if (!isGanacheRunning) {
       console.log("Ganache not running. Starting Ganache...");
-      spawn("ganache-cli", ["-p", "7545", "-i", "5777"], {
-        shell: true,
-        detached: true,
-      });
+      spawn("ganache-cli", ["-p", "7545", "-i", "5777", "--db", "C:/Users/sharm/Downloads/ganache_data"], { shell: true, detached: true });
 
-      await new Promise((resolve) => setTimeout(resolve, 5000));
+      await new Promise((resolve) => setTimeout(resolve, 5000)); // Wait for Ganache to initialize
       console.log("Ganache started successfully.");
 
-      await runTruffleMigrate();
+      // await runTruffleMigrate();
     }
 
     const contractAddress = getContractAddress();
-    const web3 = new Web3("http://127.0.0.1:7545");
     const contract = new web3.eth.Contract(ChainOfCustodyArtifact.abi, contractAddress);
 
+    console.log("Checking for blocks...");
     const numberOfBlocks = await contract.methods.getBlockCount().call();
-    if (numberOfBlocks == 0) {
-      console.log("No existing blocks found. Creating INITIAL block.");
-      const initialData = ["Initial data for the genesis block"];
-      const itemIds = [1];
-      const accounts = await web3.eth.getAccounts();
-      const account = accounts[0];
 
-      try {
-        await contract.methods.addEvidenceItems(1, itemIds, "Genesis Block", "Genesis").send({ from: account, gas: 5000000 });
-        console.log("INITIAL block created successfully.");
-      } catch (error) {
-        console.error("Error creating the INITIAL block:", error);
-      }
+    if (numberOfBlocks === '0') {
+      console.log("No blockchain file found, initializing new blockchain.");
+      // Add logic to create the INITIAL block or other initialization steps
     } else {
-      console.log("Blockchain file found with INITIAL block.");
+      console.log(`Blockchain file found with ${numberOfBlocks} blocks.`);
+      // Additional logic if the blockchain is already initialized
     }
   } catch (error) {
     console.error("Error checking for blocks:", error);
